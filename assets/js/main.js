7c3bff97ce0b09ebf92c33ef8a5f7f626ecfdeaf
shaneyoung91 <shaneadam.young@gmail.com>
2023-09-08 12:01:16 -0700

    Fix spelling errors and clean up empty folders

--- assets/js/main.js
+++ assets/js/main.js
@@ -131,7 +131,7 @@
   }, true)
 
   /**
-   * Scrool with ofset on links with a class name .scrollto
+   * Scroll with offset on links with a class name .scrollto
    */
   on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
@@ -149,7 +149,7 @@
   }, true)
 
   /**
-   * Scroll with ofset on page load with hash links in the url
+   * Scroll with offset on page load with hash links in the url
    */
   window.addEventListener('load', () => {
     if (window.location.hash) {
@@ -170,8 +170,8 @@
       strings: typed_strings,
       loop: true,
       typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
+      backSpeed: 100,
+      backDelay: 1500
     });
   }
 
